002906973a4afd73701af9e7d8fc7584e0984a7b
albapma <albaperezmejias@gmail.com>
2023-12-17 14:17:48 +0000

    react improved, bootstrap installed

--- src/App.js
+++ src/App.js
@@ -1,62 +1,41 @@
-import logo from "./logo.svg";
 import "./App.css";
 import React, { useState } from "react";
 import axios from "axios";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle";
 
 export default function App() {
   let [city, setCity] = useState("");
-  let [forecast, setForecast] = useState("");
+  let [weather, setWeather] = useState({ ready: false });
+
   function changeCity(event) {
     setCity(event.target.value);
   }
-  function SubmitEvent(event) {
+
+  function submitEvent(event) {
     event.preventDefault();
     let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=8c78e9e7e9928cd1a2a6f923072c3dec&units=metric`;
     axios.get(url).then(showForecast);
-    function showForecast(response) {
-      setForecast(
-        <div className="row">
-          <div> City {city.trim()} </div>
-          <div className="col-6">
-            <ul>
-              <li id="date"></li>
-              <li className="text-capitalize" id="weather-description">
-                {response.data.weather[0].description}{" "}
-              </li>
-            </ul>
-          </div>
-          <div className="col-6">
-            <ul>
-              <li>
-                Wind: <span id="wind"> {response.data.wind} km/h</span>
-              </li>
-              <li>
-                Humidity:{" "}
-                <span id="humidity"> {response.data.main.humidity} % </span>
-              </li>
-            </ul>
-          </div>
-          <img
-            src={`http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`}
-            alt={response.data.weather[0].description}
-            id="icon"
-          />
-          <div class="temperature-info">
-            <strong id="degrees">{Math.round(response.data.main.temp)} </strong>
-            <span class="units" id="celsius">
-              °C{" "}
-            </span>
-          </div>
-        </div>
-      );
-    }
+  }
+
+  function showForecast(response) {
+    setWeather({
+      ready: true,
+      city: response.data.name,
+      temp: Math.round(response.data.main.temp),
+      wind: Math.round(response.data.wind.speed),
+      humidity: response.data.main.humidity,
+      description: response.data.weather[0].description,
+      icon: response.data.weather[0].icon,
+      date: new Date(response.data.dt * 1000).toDateString(),
+    });
   }
 
   return (
     <div>
       <div className="Weather">
         <div className="col-6">
-          <form id="place" onSubmit={SubmitEvent}>
+          <form id="place" onSubmit={submitEvent}>
             <input
               type="text"
               placeholder="Enter your city"
@@ -66,15 +45,64 @@ export default function App() {
           </form>
         </div>
         <div className="col-6">
-          <button type="button" id="searchButton" class="btn btn-primary">
+          <button type="button" id="searchButton" className="btn btn-primary">
             Search
           </button>
 
-          <button type="button" id="current" class="btn btn-secondary">
+          <button type="button" id="current" className="btn btn-secondary">
             Current
           </button>
         </div>
-        <div> {forecast}</div>
+        {weather.ready && (
+          <div>
+            <h1> {weather.city} </h1>
+            <div className="container">
+              <div className="row">
+                <div className="col-6">
+                  <ul>
+                    <li id="date">
+                      Date: <strong>{weather.date} </strong>
+                    </li>
+                    <li className="text-capitalize" id="weather-description">
+                      Description: <strong>{weather.description}</strong>
+                    </li>
+                  </ul>
+                </div>
+                <div className="col-6">
+                  <ul>
+                    <li>
+                      Wind:{" "}
+                      <span id="wind">
+                        <strong> {weather.wind} km/h </strong>
+                      </span>
+                    </li>
+                    <li>
+                      Humidity:{" "}
+                      <span id="humidity">
+                        {" "}
+                        <strong> {weather.humidity} % </strong>
+                      </span>
+                    </li>
+                  </ul>
+                </div>
+                <div></div>
+              </div>
+              <div class="d-flex weather-temperature">
+                <img
+                  src={`http://openweathermap.org/img/wn/${weather.icon}@2x.png`}
+                  alt={weather.description}
+                  id="icon"
+                />
+                <div className="temperature-info">
+                  <strong id="degrees">{weather.temp}</strong>
+                  <span className="units" id="celsius">
+                    °C
+                  </span>
+                </div>
+              </div>
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
