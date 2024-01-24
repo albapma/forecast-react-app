d210de654df3945dcb7cecd67cb7fdb633908eac
albapma <albaperezmejias@gmail.com>
2024-01-24 04:40:49 +0000

    icon

--- src/App.js
+++ src/App.js
@@ -105,7 +105,7 @@ export default function App(props) {
               </div>
             </div>
           )}
-          <Forecast code={props.weather} />
+          <Forecast code={props.icon} />
         </div>
       </div>
     );
